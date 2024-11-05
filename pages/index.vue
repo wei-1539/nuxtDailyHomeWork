800a275507a10b94849cc36cf45d5dd72fa9fb22
Rio Wei <wutony0713@gmail.com>
2024-11-06 00:22:35 +0800

    導入bootStrap 並在首頁建立按鈕

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,18 @@
 
 <template>
   <div>
-    <h1>Page: 首頁</h1>
+    <h1 class="text-primary">Page: 首頁</h1>
     <p>目前路由的路徑 - route.fullPath 為 : /</p>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
