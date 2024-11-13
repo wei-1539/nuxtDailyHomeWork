ba5603be069b8cc3e4edcf19b87175068640e259
Rio Wei <wutony0713@gmail.com>
2024-11-13 19:20:00 +0800

    Day5  Composables指令練習

--- composables/useHome.js
+++ composables/useHome.js
@@ -1,1 +1,30 @@
-export const useHome = () => {};
+import axios from "axios";
+export const useHome = () => {
+  const newsList = ref([]);
+  const isLoading = ref(false);
+
+  const getList = async () => {
+    const url = "https://nuxr3.zeabur.app/api/v1/home/news/";
+
+    await useAsyncData("FreyjaHotelAPI", async () => {
+      isLoading.value = true;
+      try {
+        const { data } = await axios.get(url);
+        newsList.value = data.result;
+        isLoading.value = false;
+
+        return;
+      } catch (error) {
+        alert(`發生錯誤： ${error.message}`);
+      } finally {
+        isLoading.value = false;
+      }
+    });
+  };
+
+  return {
+    newsList,
+    isLoading,
+    getList,
+  };
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,22 @@
 <script setup>
 // 可以選擇使用 import 或 auto import 的方式進行導入
 // import Header from "@/components/Header.vue";
-import axios from "axios";
-const { data } = await useAsyncData("FreyjaHotelAPI", async () => {
-  const res = await axios.get("https://nuxr3.zeabur.app/api/v1/home/news/");
-  console.log(res);
 
-  return res.data;
-});
-// fetch("https://nuxr3.zeabur.app/api/v1/home/news/", { method: "GET" })
-//   .then((response) => response.json())
-//   .then((res) => {
-//     // { status: 'true', result: [{...}] }
-//     console.log(res);
-//   });
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
+const { newsList, isLoading, getList } = useHome();
+await getList();
 </script>
 <template>
   <div>
     <!-- components 裡面的 Header.vue -->
     <Header />
     <h1 class="text-primary">Page: 首頁</h1>
-    {{ data }}
-
+    {{ newsList }}
+    <NewCardDay5 v-for="item in newsList" :key="item.id" v-bind="item" />
+    <ClientOnly>
+      <Loading v-model:active="isLoading" />
+    </ClientOnly>
     <p>目前路由的路徑 - route.fullPath 為 : /</p>
   </div>
 </template>
